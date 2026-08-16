4e480d91f44c18418193832ad3d1b38b703512f7
Anmol <anmolchaudhary617@gmail.com>
2026-08-16 19:55:41 +0530

    Add the first problem solution

---
+++ first_problem.py
@@ -0,0 +1,10 @@
+first_name = "Tony"
+last_name = "Stark"
+height = 1.85
+age = 53
+
+superhero_name = input("Enter your Superhero name: ")
+
+if superhero_name.lower() == "ironman" or superhero_name.lower() == "iron man" :
+  print("Your Real name is", first_name, last_name)
+  print("You are", age, "years old and are", height, "m tall!")
