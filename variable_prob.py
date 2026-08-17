c223f224a0d7756bcfd1dc6a6a4aec438f190042
Anmol <anmolchaudhary617@gmail.com>
2026-08-17 21:22:15 +0530

    Add a simple program to illustrate use of variables

---
+++ variable_prob.py
@@ -0,0 +1,11 @@
+"""
+We check in a patient named John Smith. He is 20 years old and is a new patient.
+Create a variable for his name, age and another which tells if he is a new or an existing patient.
+"""
+patient_name = 'John Smith'
+patient_age = 20
+new_or_existing = False
+print(patient_name)
+print(patient_age)
+print('If shown false , it means he is a new patient, otherwise the patient is already existing. :')
+print(new_or_existing)
