a9d8e834e6f7d061323ec9506ba65f6066a86b79
Anmol <anmolchaudhary617@gmail.com>
2026-08-17 20:43:36 +0530

    Add a program to practice input() function

---
+++ using_input.py
@@ -0,0 +1,9 @@
+"""
+Write a program that asks the user two questions:
+User's name and his/her fav color. Then, return the message: name_of_user likes color_of_user
+"""
+user_name = input("What's your name, bro? ")
+user_color = input("And what is your favourie color? ")
+print(user_name, end=" ")
+print('likes', end=" ")
+print(user_color)
